025745770781ebfa94091dec547a5cc77c74e03b
李正彦 <limutian@163.com>
2016-06-23 12:54:11 +0800

    所有文件加入

---
+++ unit1/Assets/Script/MoveTo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+	public Transform m_transform;
+	public Vector3 camerRel;
+
+	// Use this for initialization
+	void Start () {
+
+		m_transform=this.transform;
+		camerRel=m_transform.TransformDirection(0,1,0);
+		Debug.Log(camerRel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void movotoo()
+	{
+	}
+
+}
---
+++ unit1/Assets/Script/RoteToABC.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoteToABC : MonoBehaviour {
+
+	public float target = 270.0F;
+	public float speed = 45.0F;
+	private Transform m_transform;
+	private Vector3 new_rotetion;
+
+
+	void Start()
+	{
+		m_transform=this.transform;
+		new_rotetion = Camera.main.transform.eulerAngles;
+		Debug.Log(new_rotetion.y);
+	}
+	void Update() {
+
+		Vector3 old_rotetion = this.transform.eulerAngles;
+
+		float angle = Mathf.MoveTowardsAngle(old_rotetion.y, new_rotetion.y, speed * Time.deltaTime);
+		m_transform.eulerAngles = new Vector3(0, angle, 0);
+		Debug.Log(angle+"=="+speed * Time.deltaTime+"["+old_rotetion.y+"<>"+new_rotetion.y+"]");
+		// m_transform.LookAt(Camera.main.transform);
+
+		
+
+	}
+}

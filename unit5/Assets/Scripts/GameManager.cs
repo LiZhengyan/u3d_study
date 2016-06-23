f632a23ed03abe405a65c836b18ac8653f592122
李正彦 <limutian@163.com>
2016-06-23 19:47:29 +0800

    工作结束提交

---
+++ unit5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance =null;
+	int m_score=0; //游戏得分
+	static int m_hiscore=0; //历史最高分
+
+	int m_ammo=100; //弹药数
+	playerCtrl m_player;
+	GUIText txt_ammo;
+	GUIText txt_hiscore;
+	GUIText txt_life;
+	GUIText txt_score;
+	void Start () {
+		Instance=this;
+		m_player=GameObject.FindGameObjectWithTag("Player").GetComponent<playerCtrl>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

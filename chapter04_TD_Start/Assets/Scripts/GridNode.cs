fe7501600ffc7f79336b16c99aef5362b230106f
李正彦 <75570317@qq.com>
2016-06-26 22:28:12 +0800

    睡觉前提交

--- chapter04_TD_Start/Assets/Scripts/GridNode.cs
+++ chapter04_TD_Start/Assets/Scripts/GridNode.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GridNode : MonoBehaviour {
+/*
+这个脚本定义了一个Mapdata类，。它只有一个fieldtype属性，用来表示单元格可放置状态
+因为Mapdata类并非继承自MonoBehaviour，所以不可以在inspector中被编辑，所以需要添加Serializable
+来保证它被序列话
+*/
 
-	// Use this for initialization
-	void Start () {
-	
+public class GridNode : MonoBehaviour {
+	public MapData _mapData;
+
+	void OnDrawGizmos(){
+		Gizmos.DrawIcon(this.transform.position,"gridnode.tif");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+}
+[System.Serializable]
+public class MapData
+{
+	public enum FieldTypeID{
+		GuardPosition,     //可以放置防守
+		CanNotStand,
 	}
+	public FieldTypeID fieldtype=FieldTypeID.GuardPosition;
 }
